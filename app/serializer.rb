f5ddd58105063a7ed4704ca7af36aee40a6b8e05
gyandip-chauhan <gyandip3395@gmail.com>
2022-07-05 15:23:09 +0530

    Serializing Post and Comment

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,17 @@
 class Serializer
+  def self.attributes; @attributes end
+  def self.attribute(key, &block)
+    @attributes ||= []
+    @attributes << key
+    define_method(key) { !block.nil? ? instance_eval(&block) : object.public_send(key) }
+  end
+
+  attr_reader :object
+  def initialize(object)
+    @object = object
+  end
+
+  def serialize
+    self.class.attributes.map{|i| [i, public_send(i)]}.to_h
+  end
 end
